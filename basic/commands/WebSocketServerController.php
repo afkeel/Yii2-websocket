9df5fa63a99f99b091977c069467ac2ecd39dca8
ArtemF <blademasterii@rambler.ru>
2025-01-23 15:28:48 +0500

    WebSocketServerController: start with route

--- basic/commands/WebSocketServerController.php
+++ basic/commands/WebSocketServerController.php
@@ -8,18 +8,13 @@ use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use app\websocket\Socket;
 use Yii;
+use Ratchet\App;
 
 class WebSocketServerController extends Controller {
-    public function actionStart($port = 8080) {
-        $server = IoServer::factory(
-            new HttpServer(
-                new WsServer(
-                    new Socket()
-                )
-            ),
-            $port
-        );
-        Yii::info("WebSocket server started on port $port");
-        $server->run();
-    }
+
+    public function actionStart() {
+        $app = new App('yiiprojects', 8080);
+        $app->route('/socket', new Socket(), ['*']);
+        $app->run();
+     }
 }
